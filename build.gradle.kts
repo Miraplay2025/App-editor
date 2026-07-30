48f487e4fc2f6c3bf8f72380af08508aed9b41e7
Miraplay2025 <massingueantoniocarlos38@gmail.com>
2026-07-31 00:17:43 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Configuração dos plugins raiz do Gradle
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
